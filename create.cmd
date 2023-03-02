a8aa2f6e367152d1a2e66889cc9bd6b0691b96ca
8ButtHead8 <butthead.prod@gmail.com>
2023-03-02 13:18:56 +0500

    Init

---
+++ create.cmd
@@ -0,0 +1,58 @@
+@echo off
+
+del %cd%\create
+
+set modTitleName=
+set modDescription=
+set modName=
+set rootSrc=%userprofile%\Zomboid\mods
+
+if not exist %rootSrc% (
+	echo Mod folder is not found
+	exit /b
+)
+
+set folders=\media \media\lua \media\lua\client \media\lua\server \media\lua\shared \media\models \media\scripts \media\sounds \media\textures
+
+:get_mod_name
+set /p modTitleName="Type mod name: "
+if not defined modTitleName (
+	goto get_mod_name
+)
+
+set modName=%modTitleName: =_%
+set rootSrc=%rootSrc%\%modName%
+mkdir %rootSrc%
+
+:get_mod_description
+set /p modDescription="Type mod description: "
+if not defined modDescription (
+	goto get_mod_description
+)
+
+for %%f in (%folders%) do (
+	if not exist %cd%%%f (
+		mkdir %cd%%%f
+	)
+)
+
+echo name=%modTitleName% > %cd%\mod.info
+echo id=%modName% >> %cd%\mod.info
+echo description=%modDescription% >> %cd%\mod.info
+echo poster=poster.png >> %cd%\mod.info
+echo require= >> %cd%\mod.info
+echo url= >> %cd%\mod.info
+
+for /f %%a in ('dir %cd% /b') do (
+	if not %%a==create.cmd (
+		move "%cd%\%%a" "%rootSrc%"
+	)
+)
+
+echo  > %rootSrc%\complete.cmd
+echo cd %rootSrc% >> %rootSrc%\complete.cmd
+echo del %cd%\create.cmd >> %rootSrc%\complete.cmd
+echo rmdir /q %cd% >> %rootSrc%\complete.cmd
+echo del %rootSrc%\complete.cmd >> %rootSrc%\complete.cmd
+cd ..
+call %rootSrc%\complete.cmd
